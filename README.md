a75b9e5973e00ec921d45c5476ffe480fa6a84f9
Kalaivani.Suran <kalaivani.suran@rabobank.nl>
2024-08-14 16:56:24 +0200

    added httpsecurity method httpbasic with inmemoryauthentication

--- README.md
+++ README.md
@@ -5,7 +5,21 @@ Reference project to learn and experiment springboot application
 # Idea
 ## Topic 1: Add actuator endpoint with security
 1. Done : Implement actuator endpoint and add the security to the endpoint and experiment the endpoint to expose in different port from the application
-2. TODO : CSRF, header,httpbasic and other HttpSecurity method used in filter
-3. 
+2. TODO : HttpSecurity method used in filter
+* httpbasic - Done
+* CSRF
+* header
+* anonymous()
+* rememberMe()
+* and other
+    * Sub Item A
+    * Sub Item B
+3. Authenticate with the application - with application custom properties
+
+## Random thoughts
+1. Create and release artifact
+2. JWT token validation
+3. CompletableFuture java future
+4. 
 
 
--- src/main/java/com/learning/demo/config/WebSecurityConfig.java
+++ src/main/java/com/learning/demo/config/WebSecurityConfig.java
@@ -2,13 +2,31 @@ package com.learning.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    private static final String username = "customer";
+    private static final String password = "test123";
+    private static final String DEFAULT_USER_ROLE = "User";
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        final PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        final UserDetails user = User.withUsername(username).password(encoder.encode(password)).roles(DEFAULT_USER_ROLE).build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
 
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
@@ -16,9 +34,9 @@ public class WebSecurityConfig {
 //                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/actuator/**").permitAll()
-                        .requestMatchers("/").permitAll()
+//                        .requestMatchers("/").permitAll()
                         .anyRequest().authenticated())
-//                .httpBasic(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
 //                .headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.contentSecurityPolicy(contentSecurityPolicyConfig -> contentSecurityPolicyConfig.policyDirectives("frame-ancestors 'none'")))
                 .build();
     }
